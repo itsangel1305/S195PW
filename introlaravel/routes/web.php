ea0edb919475ec97c3f2bac928f2e939b8db7605
itsangel1305 <xitsangel11@gmail.com>
2024-10-11 18:26:35 -0600

    practica 4

--- introlaravel/routes/web.php
+++ introlaravel/routes/web.php
@@ -1,12 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorVistas;
+
+
+
+Route::get('/',[controladorVistas::class,'home'])->name('rutainicio');
+
+Route::get('/formulario',[controladorVistas::class,'formulario'])->name('rutaformulario');
+
+Route::get('/clientes',[controladorVistas::class,'consulta'])->name('rutaclientes');
+
+
+
+
+
+
+
+
+
+
+
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::view('/','inicio')->name('rutainicio');
-Route::view('/formulario',view: 'formulario')->name('rutaformulario');
-Route::view('/clientes','clientes')->name('rutaclientes');
-Route::view('/componentes','componentes')->name('rutacomponentes');
+
+
+// Route::view('/','inicio')->name('rutainicio');
+// Route::view('/formulario',view: 'formulario')->name('rutaformulario');
+// Route::view('/clientes','clientes')->name('rutaclientes');
+ Route::view('/componentes','componentes')->name('rutacomponentes');
