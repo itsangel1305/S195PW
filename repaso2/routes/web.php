2671e0c165d8475b1ab792ab14e293d9a292402d
itsangel1305 <xitsangel11@gmail.com>
2024-10-31 19:31:56 -0600

    paso 5
    agregue las rutas, que en este caso fueron 3, que tienen diferentes funciones , uno para obtener los datos y otro para enviarlos y definimos las rutas con un controlador llamado Librocontroller

--- repaso2/routes/web.php
+++ repaso2/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LibroController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Ruta para la vista principal de la biblioteca
+Route::get('/', [LibroController::class, 'inicio'])->name('inicio');
+
+// Ruta para la vista de registro de libro
+Route::get('/registro', [LibroController::class, 'mostrarFormulario'])->name('registro');
+
+// Ruta para procesar el guardado del libro
+Route::post('/guardar-libro', [LibroController::class, 'almacenar'])->name('guardarLibro');
