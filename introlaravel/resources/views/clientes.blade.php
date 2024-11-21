2cda951b0a3726c0d34300889caf3ab9fddb809f
itsangel1305 <xitsangel11@gmail.com>
2024-11-20 18:13:02 -0600

    h

--- introlaravel/resources/views/clientes.blade.php
+++ introlaravel/resources/views/clientes.blade.php
@@ -1,26 +1,27 @@
+
 @extends('layouts.plantilla1')
-@section('titulo','Clientes')
-@section('contenido')
 
-<div class="container d-flex justify-content-center align-items-center" style="min-height: 88vh;"
-    @foreach($consultaClientes as $cliente)
+@section('contenido2')
+@section('titulo', 'clientes')
 
-    <class="card w-50 shadow">
-    <div class="card-header">
-        {{$cliente->nombre}}
+    <div class="container mt-5 col-md-8">
+        @foreach ($consultaClientes as $cliente)
+        <div class="card-text-justify font-monospace mt-3">
+            <div class="card-header fs-5 text-primary">
+                {{$cliente->nombre}}
+            </div>
+            <div class="card-body">
+                <h5 class="fw-bold">{{$cliente->correo}}</h5>
+                <h5 class="fw-medium">{{$cliente->telefono}}</h5>
+                <p class="card-text" fw-lighter></p>
+            </div>
+            <div class="card-footer text-muted">
+                <button type="submit" class="btn btn-warning btn-sm">actualizar</button>
+                <button type="submit" class="btn btn-danger btn-sm">eliminar</button>
+            </div>
+        </div>
+        @endforeach
     </div>
-    <div class="card-body">
-        <h5 class="fw-bold">{{$cliente->correo}}</h5>
-        <h5 class="fw-bold">{{$cliente->telefono}}</h5>
-        <p class="card-text fw-lighter"> </p>
-    </div>
-    <div class="card-footer text.muted d-flex justify-content-between">
-        <button type="submit" class="btn btn-warning btn-sm">Actualizar</button>
-        <button type="submit" class="btn btn-warning btn-sm">Eliminar</button>
-    </div>
-</div>
-@endforeach
-
-</div>
-
 @endsection
+
+
